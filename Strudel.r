e30bb100bda477dcb5d4aa4fdf255b2aeac4c4aa
Fred Ryans <18470266+gremlokk@users.noreply.github.com>
2020-07-28 10:56:53 -0500

    Update Strudel.r

--- Strudel.r
+++ Strudel.r
@@ -1,8 +1,8 @@
-# STRUDEL FIDELITY RESEARCH v3-07-25-2020
+# STRUDEL FIDELITY RESEARCH v1-07-20-2020
 # Dr. Ritter, Fredrick Ryans, Matthew Koh
 # This document is a visual analysis of the strudel data models provided
 # in the Ritter paper: fidelity on learning.
-# Last updated: 07/25/2020
+# Last updated: 07/16/2020
 
 library(dplyr)
 library(ggplot2)
@@ -10,114 +10,164 @@ library(tidyr)
 
 options(max.print=1000000)
 
-#Simple Fault Mixed Learning Plot-----------------------------------------------
-fig1 = data.frame(
-  labels = c("Low", "High", "HHap", "RWhu", "RWHap"),
-  data = c(23.3,58.75,0,0,0), 
-  sorted_labels = factor(labels, levels = c("Low", "High", "HHap", "RWhu", "RWHap"))#sorted labels (!redundant but works)
+#PRELIMINARY PLOTS -------------------------------------------------------------
+
+#RESPONSE FUNCTION GRAPH
+df = data.frame(
+  id = c(1:50),
+  values = runif(50, min=0, max=10)#Generates Random Values N=2000, i = 0-61
 )
 
-ggplot(fig1, aes(x = sorted_labels, y = data, color=sorted_labels, group= 1)) + 
-  labs(x = "Interface Type",y = "Time Per Trial") +
-  geom_line() +
-  geom_point(size = 4) +
-  ggtitle("Simple Fault Mixed Learning Plot") +
-  theme_bw()
-
-#Simple Fault Mixed Learning Plot-----------------------------------------------
-fig2 = data.frame(
-  labels = c("Low", "High", "HHap", "RWhu", "RWHap"),
-  data = c(2.6,1,0,0,0),
-  sorted_labels = factor(labels, levels = c("Low", "High", "HHap", "RWhu", "RWHap"))#sorted labels (!redundant but works)
-)
-
-ggplot(fig2, aes(x = sorted_labels, y = data, color=sorted_labels, group= 1)) + 
-  labs(x = "Interface Type",y = "Time Per Trial") +
-  geom_line() +
-  geom_point(size = 4) +
-  ggtitle("Simple Fault Mixed Learning Plot") +
-  theme_gray()
-
-#Complex Learning Curve Plot----------------------------------------------------
-
-alpha = .02
-fixedTasks = 26
-learnedTasks = 32.75
-
-fig3 = data.frame(
-  labels = c(1:10),
-  respFunc = fixedTasks + (learnedTasks*((c(1:10))^-alpha))
-)
-
-ggplot(fig3, aes(x = labels, y = respFunc, color="High", group= 1)) + 
-  labs(x = "Response Times", y = "Blocks of 10 Min.") +
-  geom_line() +
-  geom_point(size = 4) +
-  ggtitle("Complex Learning Curve Plot") +
-  theme_light()
-
-fig = data.frame(
-  labels = c(1:2000),
-  respFunc = fixedTasks + (learnedTasks*((c(1:2000))^-alpha))
-)
-
-ggplot(fig, aes(x = labels, y = respFunc, color="Lofi", group= 1)) + 
-  labs(x = "Response Times", y = "Blocks of 10 Min.") +
-  geom_line() +
-  geom_point(size = 2) +
-  ggtitle("Complex Learning Curve Plot 1-2000") +
-  theme_linedraw()
-
-
-
-#Multi-Complex Learning Curve Plot----------------------------------------------
-
-alpha = .20
-fixedTasks = 26
-learnedTasks = 32.75
-
-fig4 = data.frame(
-  labels = c(1:10),
-  respFunc = fixedTasks + (learnedTasks*((c(1:10))^-alpha)),#Function working for this
-  respFunc2 = c(40.5,37.4,36,35.2,34.6,34.1,33.8,33.5,33.2,33),#Change to working function
-  respFunc3 = c(23.3,14.5,13.1,12.4,12,11.7,11.5,11.3,11.1,11)#Change to working function
-)
-
-ggplot(fig4, aes(x = labels, color="High")) + 
-  labs(x = "Response Times", y = "Blocks of 10 Min.") +
-  geom_line(aes(y = respFunc, color="High", group= 1)) +
-  geom_line(aes(y = respFunc2, color="Low", group= 2)) +
-  geom_line(aes(y = respFunc3, color="Low-to-High", group= 3)) +
-  ggtitle("Multi-Complex Learning Curve Plot") +
-  coord_cartesian(xlim = c(1, 10), ylim = c(0, 75)) +
-  theme_light()
-
-
-#Draw-Function------------------------------------------------------------------
-#This functions draws single lines given a dataframe, x-vals, and y-vals
-
-draw = function(df,x_vals, y_vals){
-  
-  ggplot(df, aes(x = x_vals, y = y_vals, group= 1)) + 
-    labs(x = "Response Times", y = "Blocks of 10 Min.") +
-    geom_line() +
-    geom_point(size = 4) +
-    ggtitle("Complex Learning Curve Plot") +
-    theme_light()
+respFunc = function(x) {
+  alpha = .02
+  fixedTasks = 1
+  learnedTasks = x[2]
+  trial = 10
+  respTime = fixedTasks + (learnedTasks*((trial)^-alpha))
 }
 
-draw(df = fig4, x = fig4$labels, y = fig4$respFunc)#Draws graph plot
+Y = apply(df, 1, respFunc)
+
+#Create function data visualization 1-2000
+#Takes paramater of log
+ggplot(data = df, aes(x = id, y = Y, color=Y)) + 
+  labs(x = "Response Time(s)",y = "Learning Rate") +
+  geom_line() +
+  geom_point() +
+  ggtitle("Response Function Graph") +  
+  theme_classic()
+
+print(Y)
+summary(Y)
+
+#RANDOM GRAPHS
+efforts = runif(10, min=0, max=10)
+rGraph = data.frame(
+  eff = efforts,
+  Y = c(1:10)
+)
+
+ggplot(data = rGraph, aes(x = efforts, y = Y, color=Y)) +
+  labs(x = "Learning Efforts (# Attempts)",y = "Performance") +
+  geom_line() +
+  geom_point() + 
+  ggtitle("Random Graph") +
+  theme_gray()
+
+#LEARNING CURVE 1-50
+v1 = data.frame(
+  learning_efforts = c(1:50),
+  performance =  (c(1:50))^(1/2) #sqrt function x^1/2
+)
+
+ggplot(data = v1, aes(x = learning_efforts, y = performance, color = learning_efforts)) +
+  labs(x = "Learning Efforts (# Attempts)",y = "Performance") +
+  geom_line() +
+  geom_point() + 
+  ggtitle("Learning Curve 1-50") +
+  theme_classic()
+
+#LEARNING CURVE 1-2000
+v2 = data.frame(
+  learning_efforts = c(0:2000),
+  performance =  (c(0:2000))^-(1/2) #sqrt function x^1/2
+)
+
+ggplot(data = v2, aes(x = learning_efforts, y = performance, color=performance)) +
+  labs(x = "Learning Efforts (# Attempts)",y = "Performance") +
+  geom_line() +
+  geom_point() + 
+  ggtitle("Learning Curve 1-2000") +
+  theme_grey()
 
 
-#User-Input---------------------------------------------------------------------
-m = readline(prompt="Enter m value: ")
-x = readline(prompt="Enter x value: ")
-b = readline(prompt="Enter b value: ")
+#LN(X) Graph
+v3 = data.frame(
+  learning_efforts = c(0:50),
+  performance =  log(c(0:50))
+)
 
-
+ggplot(data = v3, aes(x = learning_efforts, y = performance, color=performance)) +
+  labs(x = "Learning Efforts (# Attempts)",y = "Performance") +
+  geom_line() +
+  geom_point() + 
+  ggtitle("LN(X) Plot") +
+  theme_grey() 
 
 #END----------------------------------------------------------------------------
 
+
+
+#VARIABLES----------------------------------------------------------------------
+
+alpha = .02
+fixedTasks = 1
+learnedTasks = 1
+trial = 5
+
+#FORMULAS-----------------------------------------------------------------------
+
+#Recommendations by Martin: 7/24/20
+#hifi + lofi should have their own matrix/ data-structure to generate numbers... 
+#Only apply learning rate to learning task not fixed tasks
+respTime = fixedTasks + (learnedTasks*((trial)^-alpha))
+
+#FIGURES------------------------------------------------------------------------
+
+rand_values = runif(2000, min=0, max=61)#Generates Random Values N=2000, i = 0-61
+
+fig1 = data.frame(
+  ui_combo = 	c("Lhu","LHap","Mhu","Mhap","Hhu","HHap","RWhu","RWHap"),												
+  id = c(1:8),
+  time_per_trial = c(11.75,12.95,15.95,15.95,13.75,35.4,60.25,60.25), 
+  stringsAsFactors = FALSE
+)
+
+fig2 = data.frame(
+  ui_combo = 	c("Lhu","LHap","Mhu","Mhap","Hhu","HHap","RWhu","RWHap"),												
+  id = c(1:8),
+  time_per_trial = c(4.6,4.6,3.8,3.8,1.7,1.7,1,1)
+)
+
+fig3 = data.frame(
+  time_blocks = c(1:10),
+  lofi_resp_time = c(11.8,5.5,4.3,3.8,3.5,3.2,3.1,2.9,2.8,2.7),
+  hifi_resp_time = c(13.75,7.807,5.544,4.756,4.316,4.022,3.805,3.635,3.497,3.382)
+)
+
+#PLOT VISUALIZATION-------------------------------------------------------------
+
+#results order plot in alphabetical order, we want to keep natural order
+ggplot(fig1, aes(ui_combo,time_per_trial, group = 1, color=ui_combo))+
+  geom_line(color='black',size=1) +
+  geom_point(size = 4) +
+  ylab("Times Per Trial (s)") +
+  xlab("User Interface Combination") +
+  ggtitle("Figure 1") +
+  theme_gray()
+
+#results order plot in alphabetical order, we want to keep natural order
+ggplot(fig2, aes(ui_combo,time_per_trial, group = 1, color=ui_combo))+
+  geom_line(color='black',size=1) +
+  geom_point(size = 4) +
+  ylab("Times Per Trial (s)") +
+  xlab("User Interface Combination") +
+  ggtitle("Figure 2") +
+  theme_minimal()
+
+#FIGURE 3
+ggplot(fig3, aes(x=time_blocks)) +
+  geom_line(aes(y=lofi_resp_time, color="Lofi"),size=1) + 
+  geom_line(aes(y=hifi_resp_time,color="Hifi"),size=1) + 
+  labs(x = "Response Time(s)",y = "10-Min Blocks") +
+  ggtitle("Figure 3: HiFi and LoFi Curves") +
+  theme_bw()
+
+#END----------------------------------------------------------------------------
+
+#Updates: Make more
+#Ask for user input
+#Create visualization functions for graphing (takes some data,visualize it)
 
 #SOURCES------------------------------------------------------------------------
 # [1] https://www.nngroup.com/articles/power-law-learning/#:~:text=Definition%3A%20The%20power%20law%20of,shape%20of%20a%20power%20law.
@@ -128,5 +178,5 @@ b = readline(prompt="Enter b value: ")
 # [6] https://www.mathsisfun.com/sets/functions-common.html
 # [7] https://www.valamis.com/hub/learning-curve
 # [8] https://www.statmethods.net/management/functions.html
-# [9] https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/factor
+# [9] 
 # [10]
